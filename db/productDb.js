cd774ac7837875f97beb73da14b24b6525dbf4b7
Stephen <stephenlallum@gmail.com>
2016-04-14 14:14:27 -1000

    post, put & delete complete

--- db/productDb.js
+++ db/productDb.js
@@ -5,16 +5,11 @@ var productDb = (function(){
 
 
 function getProduct(id){
-  //console.log(products);
   
   for (var i = 0; i < products.length; i++){
-    
-
     if (id == products[i].id){
       return products[i];
     }
-
-
   }
 }
 
@@ -25,17 +20,30 @@ function createProduct(productinfo){
   //console.log(productinfo);
   products.push(productinfo);
   console.log(products);
-  return true;
- 
-  
+  return true; 
 }
 
+function editProduct(id, data){
+  var product = getProduct(id);
+  product.name = data.name;
+  product.price = data.price;
+  product.inventory = data.inventory;
+  return product;
+}
 
+function deleteProduct(id, data){
+  var product = getProduct(id);
+  products.splice(product,1);
+  console.log(products);
+  return product;
+}
 
 
 return {
   createProduct: createProduct,
-  getProduct: getProduct
+  getProduct: getProduct,
+  editProduct: editProduct,
+  deleteProduct: deleteProduct
 };
 
 
--- routes/products.js
+++ routes/products.js
@@ -3,16 +3,17 @@ var productsRoute = express.Router();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var ProductDb = require('../db/productDb');
-
-
 productsRoute.use(bodyParser.urlencoded({
   extended: true
 }));
+
+//get products
 
 productsRoute.get('/'), function(req, res){
 	ProductDb.getProduct();
 };
 
+//create new product and store to persProds
 
 productsRoute.post('/', function(req, res) {
 
@@ -22,21 +23,24 @@ productsRoute.post('/', function(req, res) {
 	}
 });
 
+//find and modify an item by id
 
 productsRoute.put('/:id', function(req, res){
 
 	var productId = req.params.id;
-  ProductDb.getProduct(productId);
+  ProductDb.editProduct(productId, req.body);
  	res.send({'success': true});
- 
+
 });
+
+//find and delete an item by id
 
 productsRoute.delete('/:id', function(req, res){
 
-	var productData = req.body;
-
-
-
+	var productId = req.params.id;
+	ProductDb.deleteProduct(productId, req.body);
+	
+	
 });
 
 
